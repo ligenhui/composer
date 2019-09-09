a69f1da97ac4b00b5be34ca64b79339ed8d13459
Administrator <657801509@qq.com>
2019-09-09 14:40:14 +0800

    insert TpPaginator Class

---
+++ src/TpPaginator.php
@@ -0,0 +1,89 @@
+<?php
+
+
+namespace Ligenhui\Package;
+
+
+class TpPaginator extends \think\paginator\driver\Bootstrap
+{
+    /**
+     * 页码按钮
+     * @return string
+     */
+    protected function getLinks()
+    {
+        if ($this->simple)
+            return '';
+
+        $block = [
+            'first'  => null,
+            'slider' => null,
+            'last'   => null
+        ];
+
+        $side   = 1;
+        $window = $side * 2;
+
+        if ($this->lastPage < $window + 6) {
+            $block['first'] = $this->getUrlRange(1, $this->lastPage);
+        } elseif ($this->currentPage <= $window) {
+            $block['first'] = $this->getUrlRange(1, $window + 2);
+            $block['last']  = $this->getUrlRange($this->lastPage - 1, $this->lastPage);
+        } elseif ($this->currentPage > ($this->lastPage - $window)) {
+            $block['first'] = $this->getUrlRange(1, 2);
+            $block['last']  = $this->getUrlRange($this->lastPage - ($window + 2), $this->lastPage);
+        } else {
+            $block['first']  = $this->getUrlRange(1, 2);
+            $block['slider'] = $this->getUrlRange($this->currentPage - $side, $this->currentPage + $side);
+            $block['last']   = $this->getUrlRange($this->lastPage - 1, $this->lastPage);
+        }
+
+        $html = '';
+
+        if (is_array($block['first'])) {
+            $html .= $this->getUrlLinks($block['first']);
+        }
+
+        if (is_array($block['slider'])) {
+            $html .= $this->getDots();
+            $html .= $this->getUrlLinks($block['slider']);
+        }
+
+        if (is_array($block['last'])) {
+            $html .= $this->getDots();
+            $html .= $this->getUrlLinks($block['last']);
+        }
+
+        return $html;
+    }
+
+    /**
+     * 渲染分页html
+     * @return mixed
+     */
+    public function render()
+    {
+        if ($this->hasPages()) {
+            if ($this->simple) {
+                return sprintf(
+                    '<ul class="pager">%s %s</ul>',
+                    $this->getPreviousButton(),
+                    $this->getNextButton()
+                );
+            } else {
+                $string = '<div class="dt-toolbar-footer">
+								<div class="col-sm-6 col-xs-12 hidden-xs">
+									<div class="dataTables_info">%d条记录，第%d/%d页 </div>
+								</div>
+								<div class="col-xs-12 col-sm-6">
+									<div class="dataTables_paginate">
+										<ul class="pagination">%s %s %s</ul>
+									</div>
+								</div>
+							</div>';
+
+                return sprintf($string, $this->total(),$this->currentPage(),$this->lastPage(),$this->getPreviousButton('上一页'),$this->getLinks(),$this->getNextButton('下一页'));
+            }
+        }
+    }
+}
